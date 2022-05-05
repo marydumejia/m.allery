8502d2f79cdb9e1461fe3e3876f2fdc54e25f420
Mary Duarte Mejia <marysita@Marys-MacBook-Air.local>
2022-05-05 06:01:06 -0500

    secons

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ const imageContainer = document.getElementById('image-container')
 const imageFile = document.getElementById('img-pic')
 const imageAdd = document.getElementById('add-img')
 const contentTable = document.getElementById('content-choose')
+const introText = document.getElementById('intro-text')
 
 const imageTitle = document.getElementById('img-title')
 const imageUrl = document.getElementById('img-url')
@@ -13,13 +14,15 @@ const beforeButton = document.getElementById('before-button');
 const afterButton = document.getElementById('after-button');
 
 uploadImg.addEventListener('click', () => {
-    contentTable.parentElement.classList.remove('hidden');
+    contentTable.classList.remove('hidden');
+    introText.classList.add('hidden');
 })
 
 uploadUrl.addEventListener('click', ()=>{
-    contentTable.parentElement.classList.remove('hidden');
+    contentTable.classList.remove('hidden');
     imageUrl.classList.remove('hidden');
-    imageFile.classList.add('hidden');
+    imageFile.parentElement.classList.add('hidden');
+    introText.classList.add('hidden');
 })
 
 beforeButton.addEventListener('click', () => {
@@ -28,6 +31,15 @@ beforeButton.addEventListener('click', () => {
         setCurrentPage(currentPage - 1);
     }
 });
+
+function getCurrentPage () {
+    const query = location.href.split('?')[1];
+    return query ? Number(query.split('=')[1]) : 1;
+}
+
+function setCurrentPage (page) {
+    location.href = `?page=${page}`;
+}
 
 afterButton.addEventListener('click', () => {
     const currentPage = getCurrentPage();
@@ -58,6 +70,89 @@ imageAdd.addEventListener('click', async () => {
     cleanInputs();
     clearContainer(imageContainer);
     createImages(images, imageContainer);
-    createPages(images, getCurrentPage());
 });
 
+function getLocalImages () {
+    return JSON.parse(localStorage.getItem('gallery')) || [];
+}
+
+function setLocalImages (images) {
+    localStorage.setItem('gallery', JSON.stringify(images));
+}
+
+function cleanInputs () {
+    imageFile.value = '';
+    imageUrl.value = '';
+    imageTitle.value = '';
+}
+
+function clearContainer (container) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+function createImages (images,container) {
+    const sliced = slicedImages(images, getCurrentPage());
+    sliced.forEach(image => {
+        const div = document.createElement('div');
+        const img = document.createElement('img');
+        const title = document.createElement('h3');
+        const descrip = document.createElement('h1');
+
+        div.className = 'w-full h-auto cursor-pointer';
+
+        img.src = image.url;
+        img.alt = image.title;
+        img.title = image.title;
+        img.descrip = image.descrip;
+        img.className = 'w-full h-64 object-cover rounded-lg hover:h-[22rem] transition-[height] ease-in-out';
+
+        title.innerText = image.title;
+        descrip.innerText = image.descrip;
+        title.className = 'text-center text-2xl text-gray-800';
+
+        div.appendChild(img);
+        div.appendChild(title);
+        div.appendChild(descrip);
+        container.appendChild(div);
+    });
+}
+
+function slicedImages (images, page) {
+    const start = (page - 1) * 3;
+    const end = page * 3;
+    return images.slice(start, end);
+}
+
+function getDataURL (file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            const image = new Image();
+            image.onload = () => {
+                resolve(resizeImage(image, file.type));
+            };
+            image.src = reader.result.toString();
+        };
+        reader.onerror = () => reject(reader.error);
+        reader.readAsDataURL(file);
+    });
+}
+
+function resizeImage (image, imageType) {
+    const MAX_WIDTH = 600;
+    const MAX_HEIGHT = 400;
+
+    const ratio = Math.min(MAX_WIDTH / image.width, MAX_HEIGHT / image.height);
+    const width = image.width * ratio;
+    const height = image.height * ratio;
+
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = width;
+    canvas.height = height;
+    ctx.drawImage(image, 0, 0, width, height);
+
+    return canvas.toDataURL(imageType);
+}
